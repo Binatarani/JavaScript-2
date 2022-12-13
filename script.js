01e3acfd80e4f24ecb60534cbe7c4909c14235ea
Binatarani <binatarani.bd@gmail.com>
2022-12-13 20:36:14 +0600

    Class two

---
+++ script.js
@@ -0,0 +1,167 @@
+//let firstName = "Asif";
+//let age = 33;
+//console.log(firstName);
+//console.log(age);
+
+//A. object
+//B. primitive
+/*
+//A. object
+1. Number
+2. String
+3. Boolean
+4. Undefind
+5. Null
+6. Symble
+7. Bigint
+*/
+
+// //1. Number;
+// let age = 29;
+// console.log(age);
+
+// //2. String
+// let firstName = "Shingdho";
+// console.log(firstName);
+
+//3. Boolean
+// let isSinger = true;
+// console.log(isSinger);
+
+// //4.Undefind
+// let homeCity;
+// console.log(homeCity);
+
+// //5. Null
+// let income = null;
+// console.log(null);
+
+// console.log(age);
+
+//Type of varieble
+
+//1.type of oparetor
+
+// console.log(typeof age);
+// console.log(typeof homeCity);
+// console.log(typeof isSinger);
+
+// console.log(homeCity);
+// homeCity = "Rajshahi";
+// console.log(homeCity);
+
+// console.log(isSinger);
+// isSinger = "Bangladesh Singer";
+// console.log(isSinger);
+
+// console.log(typeof isSinger);
+// isSinger = "Bangladesh Singer";
+// console.log(typeof isSinger);
+// console.log(typeof income); //null everytime console show object.//javascript bug
+
+//Three types variable
+//1. let
+//2. const
+//3. var
+
+//let firstName = "Asif";
+
+// //let
+// let myAge = 33;
+// console.log(myAge);
+// myAge = 35;
+// console.log(myAge);
+
+// //const
+// const birthyear = 1988;
+// console.log(birthyear);
+
+// //var
+// var job = "teacher";
+// console.log(job);
+// job = "doctor";
+// console.log(job);
+
+//Type of javaScript
+
+// // 1. Arithmetic operators
+
+// console.log(16 - 12);
+// let profit = 32 - 14;
+// console.log(profit);
+
+// let myAge = 2022 - 1988;
+// console.log(myAge);
+// myAge = 2023 - 1988;
+// console.log(myAge);
+
+// let adriAge = 2022 - 2019;
+// console.log(adriAge);
+
+// let currenYear = 2023;
+
+// let myAge = currenYear - 1988;
+// console.log(myAge);
+
+// let adriAge = currenYear - 2019;
+// console.log(myAge, adriAge);
+// ageDiff = myAge - adriAge;
+// console.log(ageDiff);
+// +
+// -
+// *
+// /
+// **
+// %
+// ++
+// // --
+// console.log(10 + 5);
+// console.log(10 - 5);
+// console.log(5 * 5);
+// console.log(25 / 5);
+// console.log(3 ** 3);
+// console.log(25 % 3);
+
+// let x = 10;
+// x++;
+// console.log(x);
+
+// let y = 20;
+// y--;
+// console.log(y);
+
+// console.log(100 - 20 + (20 * 2) / 8);
+// let firstName = "Asif";
+// let lastName = "Abir";
+// console.log(firstName + " " + lastName);
+
+// // 2. Assignment operators
+
+// // =
+// // +=
+// // -=
+// // *=
+// // /=
+// let item = 10 + 5;
+// console.log(item);
+
+// let x = 15;
+// x += 10; //x= x+10;
+// //x = 15+10
+// console.log(x);
+
+// let y = 20;
+// y -= 10; //y= y-10;
+// //y = 20-10;
+// console.log(y);
+
+// let z = 5;
+// z *= 5; //z = z * 5;
+// //z = 5 * 5;
+// //z = 25;
+// console.log(z);
+
+// let w = 15;
+// w /= 3; //w = w / 3;
+// //w = 15 / 3
+// console.log(w);
